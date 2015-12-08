81d2b6564800e234eaf73183975668b56ae96ead
firmread <litchirhythm@gmail.com>
2015-12-07 23:33:29 -0500

    clean up comments

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -32,22 +32,6 @@ void ofApp::setup(){
         else if(abs(i-500)%8 == 7) stripes.push_back(y);
         else stripes.push_back(w);
     }
-//    
-//    stripes.push_back(y);
-//    stripes.push_back(p);
-//    stripes.push_back(b);
-//    
-//    stripes.push_back(k);
-//    
-//    stripes.push_back(b);
-//    stripes.push_back(p);
-//    stripes.push_back(y);
-//    
-//    stripes.push_back(w);
-//    
-//    stripes.push_back(y);
-//    stripes.push_back(p);
-//    stripes.push_back(b);
 }
 
 //--------------------------------------------------------------
@@ -58,34 +42,6 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    //v1
-//    int counter = 0;
-//    for (int i = 0; i < 200; i++) {
-//        ofSetColor(counter);
-//        ofDrawRectangle(i*20, 0, 20, ofGetHeight());
-//        counter += 10;
-//        if(counter > 255) counter = 0;
-//    }
-//    
-//    ofSetColor(y.c);
-//    ofDrawCircle(mouseX,mouseY,10);
-
-    //v2
-//    stripes[0].x = mouseX;
-//    ofSetColor(stripes[0].c);
-//    ofDrawRectangle(stripes[0].x, 0, stripes[0].width*width, ofGetWidth());
-//    
-//    
-//    for (int i = 1; i< stripes.size();i++) {
-//        ofSetColor(stripes[i].c);
-//        stripes[i].x = stripes[i-1].x + stripes[i-1].width*width;
-//        ofDrawRectangle(stripes[i].x, 0, stripes[i].width*width, ofGetHeight());
-//    }
-//    
-//    ofSetColor(sin(ofGetElapsedTimef()*10)*100, 0,0);
-//    ofDrawRectangle(stripes[stripes.size()/2].x, 0, stripes[stripes.size()/2].width*width, ofGetWidth());
-    
-    //v3
     stripes[500].x = mouseX;
     ofSetColor(stripes[500].c);
     ofDrawRectangle(stripes[500].x, 0, stripes[500].width*width, ofGetWidth());
